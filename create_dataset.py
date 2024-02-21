ba1ec0c0f0c1e8f0299129310b2916e58b9bd6bf
Arpit Anand <=>
2024-02-21 15:41:10 +0530

    Modified create_dataset to include config.both_transform

--- create_dataset.py
+++ create_dataset.py
@@ -2,10 +2,10 @@ import os
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import numpy as np 
-import sys
+import config
 
 
-path_to_search = " C:\Projects\Manga_Illustration_pix2pix\\train"
+path_to_search = "C:\Projects\Manga_Illustration_pix2pix\\train"
 
 class CustomDataset(Dataset):
     def __init__(self, file_path, transform=None):
@@ -20,18 +20,21 @@ class CustomDataset(Dataset):
         img_path = self.file_path[index]
         image = np.array(Image.open(img_path).convert("RGB"))
         width = image.shape[2]
+        
+        # to add any additional trasnforms as compared to the config.both_transforms 
         if self.transform:
             image = self.transform(image)
 
         # Split the image into input and target
         input_image = image[:, :width//2, :]
         target_image = image[:, width//2:, :]
+        augmentations = config.both_transform(image=input_image, target=target_image)
+        input_image = augmentations["image"]
+        target_image = augmentations["target"]
+
         
         return input_image, target_image
 
 dataset = CustomDataset(path_to_search)
-loader = DataLoader(dataset, batch_size=5)
-for x, y in loader:
-        print(x.shape)
-        sys.exit()
+loader = DataLoader(dataset, batch_size=config.BATCH_SIZE)
 
